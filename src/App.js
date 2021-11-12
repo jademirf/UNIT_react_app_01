e7bb6ad42dffc6b960b9ef4c5ccd23d63dfab2a8
Jademir de Moura <jademirf@gmail.com>
2021-11-12 20:56:00 -0300

    criando lista a partir do todo do jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css';
 //importar o servico axios "api"
 import api from './services/api'
 
 function App() {
+  // iniciar um array vazio pra guardar a lista de tasks
+  const [tasks, setTasks] = useState([])
+
   useEffect(() => {
     api.get('todos').then( response => {
-      console.log(response.data)
+      setTasks(response.data)
     })
   }, [])
 
@@ -15,19 +17,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo List - React</h1>
       </header>
+      <main>
+        <ul>
+          { 
+            tasks.map(task => {
+              return (
+                <li key={task.id}>
+                  <h5>{task.title}</h5>
+                  <input type="checkbox" checked={task.completed} />
+                </li>
+              )
+            })
+          }
+        </ul>
+      </main>
     </div>
   );
 }
