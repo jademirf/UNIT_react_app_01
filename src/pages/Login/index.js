c19afee1c5b623c588e797f3a610b7eff669f8c5
Jademir de Moura <jademirf@gmail.com>
2021-11-26 21:11:50 -0300

    add react-bootstrap components

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,52 @@
+import React, { useState} from 'react'
+import { Card, Col, Container, Row, Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+
 import Header from '../../components/Header'
-import FooterMenu from '../../components/FooterMenu'
 
 import './style.css'
 
 const Home = () => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Envivou o login e senha', {login, password})
+  }
 
   return (
     <div className="App">
       <Header listTitle={'Estudos'}/>
-      <main>
-        Login page!
-      </main>
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <h3>Faça seu login:</h3>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control value={login} onChange={e => setLogin(e.target.value)} type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                  </Form.Group>
+                  <Button onClick={handleLogin}variant="primary" type="submit">
+                    Login
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
